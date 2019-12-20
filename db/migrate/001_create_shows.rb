4fc40fe34d14d20b2124f78bdf27bde2a1caa0e0
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-12-20 19:27:02 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -3,8 +3,9 @@ class CreateShows < ActiveRecord::Migration[5.2]
     create_table :shows do |t|
       t.string :name 
       t.string :network
-      t.timestamp :day
+      t.string :day
       t.integer :rating
     end
   end
 end
+
